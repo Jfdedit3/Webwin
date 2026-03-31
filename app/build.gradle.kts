8dffe16d484f892bc029f15f7f97fd348945805c
z4trox <kylian.lefebvre60@gmail.com>
2026-03-31 16:34:38 +0200

    Remove unused androidx.webkit dependency to fix Android CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,5 +46,4 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.2")
-    implementation("androidx.webkit:webkit:1.11.0")
 }
